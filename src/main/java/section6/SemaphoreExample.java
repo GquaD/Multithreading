dfc57c85d3dc7339a8c46262d1f7e1b545dd24c9
Behzod Tairov <tbekpro@gmail.com>
2023-07-26 21:35:24 -0400

    - AtomicInteger example - Semaphore example

---
+++ src/main/java/section6/SemaphoreExample.java
@@ -0,0 +1,42 @@
+package section6;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+enum Downloader {
+    INSTANCE;
+    private Semaphore semaphore = new Semaphore(3, true);
+
+    public void download() {
+        try {
+            semaphore.acquire();
+            downloadData();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void downloadData() {
+        try {
+            System.out.println("Downloading data from the web...");
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+public class SemaphoreExample {
+    public static void main(String[] args) {
+        //create multiple threads - executors
+        ExecutorService service = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 12; i++) {
+            service.execute(() -> Downloader.INSTANCE.download());
+        }
+    }
+}
